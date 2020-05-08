ca69d5c69b21f3424e9ea28773fe9a75cb9aacf2
amiralitaheri <amiralitaheri64@gmail.com>
2020-05-08 22:27:50 +0430

    fixed wrapper

--- src/utils/status_rate_wrapper.py
+++ src/utils/status_rate_wrapper.py
@@ -6,29 +6,26 @@ class StatusRateWrapper:
     def __str__(self):
         return f'Number is {self.rate}'
 
-    def __f(self, x):
-        return x % 16
-
     def __lt__(self, obj):
         """self < obj."""
-        return self.__f(self.rate) < self.__f(obj.rate)
+        return self.rate < obj.rate
 
     def __le__(self, obj):
         """self <= obj."""
-        return self.__f(self.rate) <= self.__f(obj.rate)
+        return self.rate <= obj.rate
 
     def __eq__(self, obj):
         """self == obj."""
-        return self.__f(self.rate) == self.__f(obj.rate)
+        return self.rate == obj.rate and self.status.id == obj.status.id
 
     def __ne__(self, obj):
         """self != obj."""
-        return self.__f(self.rate) != self.__f(obj.rate)
+        return self.rate != obj.rate or self.status.id != obj.status.id
 
     def __gt__(self, obj):
         """self > obj."""
-        return self.__f(self.rate) > self.__f(obj.rate)
+        return self.rate > obj.rate
 
     def __ge__(self, obj):
         """self >= obj."""
-        return self.__f(self.rate) >= self.__f(obj.rate)
+        return self.rate >= obj.rate

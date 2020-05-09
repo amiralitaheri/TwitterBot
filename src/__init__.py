6c78dad4187db5e174a0711a1e562cad3b2e4ad4
amiralitaheri <amiralitaheri64@gmail.com>
2020-05-09 12:36:04 +0430

    fixed logging

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,16 @@
 import logging
 import sys
 
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 # code for logging to console
 console = logging.StreamHandler(sys.stdout)
+console.setFormatter(formatter)
 logging.root.addHandler(console)
 
 # Code for logging to file
 file = logging.FileHandler('bot.log', encoding='utf-8')
+file.setFormatter(formatter)
 logging.root.addHandler(file)
+
 logging.root.setLevel(logging.WARNING)
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s')

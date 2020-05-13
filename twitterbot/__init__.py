30e69b20d8db6ff95aabb2615eee0a46bf6d1509
amiralitaheri <amiralitaheri64@gmail.com>
2020-05-14 01:27:18 +0430

    added telegram logger

--- twitterbot/__init__.py
+++ twitterbot/__init__.py
@@ -1,6 +1,8 @@
 import logging
 import sys
 
+import requests
+
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # code for logging to console
@@ -12,5 +14,27 @@ logging.root.addHandler(console)
 file = logging.FileHandler('bot.log', encoding='utf-8')
 file.setFormatter(formatter)
 logging.root.addHandler(file)
+
+# code for logging to telegram
+TELEGRAM_TOKEN = ''
+TELEGRAM_CHAT_ID = ''
+
+
+class RequestsHandler(logging.Handler):
+    def emit(self, record):
+        log_entry = self.format(record)
+        payload = {
+            'chat_id': TELEGRAM_CHAT_ID,
+            'text': log_entry,
+            'parse_mode': 'HTML'
+        }
+        return requests.post("https://api.telegram.org/bot{token}/sendMessage".format(token=TELEGRAM_TOKEN),
+                             data=payload).content
+
+
+if TELEGRAM_TOKEN != '' and TELEGRAM_CHAT_ID != '':
+    telegram = RequestsHandler()
+    telegram.setFormatter(formatter)
+    logging.root.addHandler(telegram)
 
 logging.root.setLevel(logging.WARNING)

a543c1ad8af3f10577020f162adb5018e6888924
amiralitaheri <amiralitaheri64@gmail.com>
2020-05-18 02:47:56 +0430

    added simple voting system

--- twitterbot/telegram/telegram.py
+++ twitterbot/telegram/telegram.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import requests
@@ -13,5 +14,20 @@ class Telegram:
             'text': tweet_link,
             'parse_mode': 'HTML'
         }
-        logging.info(requests.post("https://api.telegram.org/bot{token}/sendMessage".format(token=bot_token),
-                                   data=payload).content)
+        logging.warning(requests.post("https://api.telegram.org/bot{token}/sendMessage".format(token=bot_token),
+                                      data=payload).content)
+        Telegram.send_poll(status.id_str, bot_token, chat_id, ['funny', 'useful', 'offensive'])
+
+    @staticmethod
+    def send_poll(tweet_id: str, bot_token: str, chat_id: str, options: list):
+        payload = {
+            'chat_id': chat_id,
+            'question': '⬆⬆' + tweet_id + '⬆⬆',
+            'options': json.dumps(options),
+            'is_anonymous': True,
+            'allows_multiple_answers': True,
+            'parse_mode': 'HTML'
+        }
+
+        logging.warning(requests.post("https://api.telegram.org/bot{token}/sendPoll".format(token=bot_token),
+                                      data=payload).content)

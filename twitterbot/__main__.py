db97df0834122063d0ef7097c2628a70884c3321
amiralitaheri <amiralitaheri64@gmail.com>
2020-10-18 12:52:47 +0330

    removed retweet schedule, retweet on sight

--- twitterbot/__main__.py
+++ twitterbot/__main__.py
@@ -4,44 +4,18 @@ import sys
 import time
 import traceback
 from datetime import datetime
-from queue import PriorityQueue
 
 import tweepy
 from apscheduler.schedulers.background import BackgroundScheduler
 
 from twitterbot.storagehandlers.json_storage_handler import JsonStorageHandler
-from twitterbot.telegram.telegram import Telegram
 from twitterbot.tweetselectors.greedy_selector import GreedySelector
 from twitterbot.twitter.authentication import authenticate_1
 from twitterbot.twitter.tweet_listener import TweetListener
 from twitterbot.utils.config import Config
-from twitterbot.utils.status_rate_wrapper import StatusRateWrapper
 
 
-# this function will be called in intervals and will pop the top tweet from selected_tweets and retweet it
-def retweet_function(selected_tweets, api):
-    succeeded = False
-    config = Config()
-    while True:
-        try:
-            if selected_tweets.qsize() == 0:
-                break
-            wrapper: StatusRateWrapper = selected_tweets.get(block=False)
-            logging.info('retweeting message with rating(' + str(wrapper.rate * -1) + '): ' + str(wrapper.status.id))
-            api.retweet(wrapper.status.id)
-            succeeded = True
-            if selected_tweets.qsize() < 10:
-                break
-        except tweepy.error.TweepError:
-            logging.info('You have already retweeted this Tweet.')
-    if config.TELEGRAM and succeeded:
-        try:
-            Telegram.post_tweet_link(wrapper.status, config.TELEGRAM_BOT_TOKEN, config.TELEGRAM_CHANNEL_ID)
-        except Exception:
-            logging.error("Can't post on telegram")
-
-
-def stream_tweets(selected_tweets: PriorityQueue, auth: tweepy.OAuthHandler) -> None:
+def stream_tweets(auth: tweepy.OAuthHandler, api) -> None:
     config = Config()
     if not config.TRACKS:
         config.TRACKS = {'twitter': 0.9}
@@ -52,11 +26,11 @@ def stream_tweets(selected_tweets: PriorityQueue, auth: tweepy.OAuthHandler) -> 
                     + '\nsave_tweets_path: ' + config.SAVE_TWEETS_PATH
                     + '\ntracks: ' + " ".join(config.TRACKS.keys()))
     try:
-        tweet_selector = GreedySelector(config.TRACKS, config.FILTER_WORDS, config.BLACK_LIST)
+        tweet_selector = GreedySelector(api, config.TRACKS, config.FILTER_WORDS, config.BLACK_LIST)
         storage_handler = None
         if config.SAVE_TWEETS:
             storage_handler = JsonStorageHandler(config.SAVE_TWEETS_PATH)
-        listener = TweetListener(selected_tweets, tweet_selector, storage_handler)
+        listener = TweetListener(tweet_selector, api, storage_handler)
         stream = tweepy.Stream(auth=auth, listener=listener)
         # starting stream
         tracks: list = list(config.TRACKS.keys())
@@ -70,19 +44,17 @@ def stream_tweets(selected_tweets: PriorityQueue, auth: tweepy.OAuthHandler) -> 
 def keyboard_interrupt_handler(signal_input, frame):
     logging.error("KeyboardInterrupt (ID: {}) has been caught. exiting".format(signal_input))
     stream_scheduler.shutdown(wait=False)
-    retweet_scheduler.shutdown(wait=False)
     exit(0)
 
 
 def main():
     config = Config()
-    selected_tweets = PriorityQueue()
 
     auth = authenticate_1(config.CONSUMER_KEY, config.CONSUMER_SECRET, config.TOKEN_KEY, config.TOKEN_SECRET)
     api = tweepy.API(auth)
 
     stream_scheduler.add_job(stream_tweets,
-                             args=[selected_tweets, auth],
+                             args=[auth, api],
                              coalesce=True,
                              trigger='interval',
                              minutes=20,
@@ -92,17 +64,6 @@ def main():
                              next_run_time=datetime.now(),
                              id='stream_scheduler')
     stream_scheduler.start()
-
-    # scheduler to call retweet_function in intervals
-    retweet_scheduler.add_job(retweet_function,
-                              args=[selected_tweets, api],
-                              trigger='interval',
-                              minutes=config.RETWEET_INTERVAL,
-                              misfire_grace_time=15,
-                              max_instances=1,
-                              name='retweet_scheduler',
-                              id='retweet_scheduler')
-    retweet_scheduler.start()
 
     # registering interrupt handler
     signal.signal(signal.SIGINT, keyboard_interrupt_handler)
@@ -116,7 +77,6 @@ def main():
 
 if __name__ == "__main__":
     stream_scheduler = BackgroundScheduler()
-    retweet_scheduler = BackgroundScheduler()
     main()
 
     # auth = authenticate_2(config.CONSUMER_KEY, config.CONSUMER_SECRET)
--- twitterbot/twitter/tweet_listener.py
+++ twitterbot/twitter/tweet_listener.py
@@ -1,24 +1,24 @@
 import logging
 import threading
 import time
-from queue import Queue, PriorityQueue
+from queue import Queue
 
 import tweepy
 
 from twitterbot.abstracts.storage_handler_interface import StorageHandlerInterface
 from twitterbot.abstracts.tweet_selector_interface import TweetSelectorInterface
-from twitterbot.utils.status_rate_wrapper import StatusRateWrapper
+from twitterbot.telegram.telegram import Telegram
+from twitterbot.utils.config import Config
 
 
 class TweetListener(tweepy.StreamListener):
-    def __init__(self, queue: PriorityQueue, selector: TweetSelectorInterface,
+    def __init__(self, selector: TweetSelectorInterface, api,
                  storage_handler: StorageHandlerInterface = None):
         super().__init__()
-        self.queue = queue
         self.selector = selector
         self.storage_handler = storage_handler
         self.fifo = Queue()
-        self.executor = Executor(self.fifo, queue, selector, storage_handler)
+        self.executor = Executor(self.fifo, selector, storage_handler, api)
 
     def on_status(self, status):
         self.fifo.put_nowait(status)
@@ -44,15 +44,15 @@ class TweetListener(tweepy.StreamListener):
 
 
 class Executor(threading.Thread):
-    def __init__(self, fifo: Queue, queue: PriorityQueue, selector: TweetSelectorInterface,
-                 storage_handler: StorageHandlerInterface):
+    def __init__(self, fifo: Queue, selector: TweetSelectorInterface,
+                 storage_handler: StorageHandlerInterface, api):
         super().__init__()
         self.fifo = fifo
-        self.queue = queue
         self.selector = selector
         self.storage_handler = storage_handler
         self.killer = False
         self.tweet_counter = 0
+        self.api = api
 
     def run(self):
         while True:
@@ -69,7 +69,18 @@ class Executor(threading.Thread):
 
         # put tweets on queue for main telegram channel and twitter
         if rating > 0.61:  # only add tweets with rating above 0.61
-            wrapper = StatusRateWrapper()
-            wrapper.status = status
-            wrapper.rate = -1 * rating  # (-1 * rating) because python PQ uses min-heap(min value will pop first)
-            self.queue.put(wrapper)
+            retweet(status, self.api)
+
+
+def retweet(status, api):
+    succeeded = True
+    config = Config()
+    try:
+        api.retweet(status.id)
+    except tweepy.error.TweepError:
+        succeeded = False
+    if config.TELEGRAM and succeeded:
+        try:
+            Telegram.post_tweet_link(status, config.TELEGRAM_BOT_TOKEN, config.TELEGRAM_CHANNEL_ID)
+        except Exception:
+            logging.error("Can't post on telegram")
